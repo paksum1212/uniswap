6b23e8a2c97b7e27926a1813bb2e083a02c2fbb5
Sam Wong <sampaksum1212@gmail.com>
2021-04-24 17:51:19 +0800

    Develop topbar control and layout

--- src/App.vue
+++ src/App.vue
@@ -19,12 +19,65 @@ export default {
 </script>
 
 <style>
+html {
+  width: 100%;
+  height: 100%;
+  background: #072654;
+  background-image: radial-gradient(farthest-corner at 30% 5%, #062458 0%, #232429 100%);
+}
+body { 
+  padding: 0px;
+  margin: 0px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #ffffff;
+  margin: 30px auto;
+  padding: 10px;
+  max-width: 1280px;
+}
+
+/* Common */
+.btn-add {
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border: 2px solid #ffffff;
+  border-radius: 50%;
+  vertical-align: middle;
+  cursor: pointer;
+}
+.btn-add.disabled {
+  border-color: #1A539A;
+  color: #1A539A;
+  cursor: default;
+}
+.btn-add::after {
+  content: '+';
+  width: 100%;
+  height: 100%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: block;
+  line-height: 100%;
+}
+.btn-std {
+  display: inline-block;
+  height: 16px;
+  padding: 8px 16px;
+  border-radius: 8px;
+  background: #2271E7;
+  color: #ffffff;
+  font-size: 14px;
+  line-height: 16px;
+  vertical-align: middle;
+  cursor: pointer;
+}
+.btn-std.disabled {
+  color: #1A539A;
+  background: #192C4C;
+  cursor: default;
 }
 </style>
--- src/components/Header/TopBar.vue
+++ src/components/Header/TopBar.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="top-bar">
-    {{ title }}
+    <div>
+      <span class="icon"><img v-bind:src="images.icon(this.$code1)" /></span>
+      <span class="icon"><img v-bind:src="images.icon(this.$code2)" /></span>
+      <span class="title">{{ title }}</span>
+    </div>
+    <div>
+      <div class="btn-add" v-bind:class="{ disabled: !enable.add }"></div>
+      <div class="btn-std" v-bind:class="{ disabled: !enable.addLiquidity }">+ Add Liquidy</div>
+      <div class="btn-std" v-bind:class="{ disabled: !enable.trade }">Trade</div>
+    </div>
   </div>
 </template>
 
@@ -10,10 +19,54 @@ export default {
   props: {
     title: String,
     enable: Object
+  },
+  data() {
+    return {
+      images: {
+        icon: (code) => require('@/assets/icons/' + code + '.png')
+      }
+    }
   }
 }
 </script>
 
 <style scoped>
+.top-bar {
+  position: relative;
+  margin-bottom: 20px;
+}
+.top-bar > div {
+  display: inline-block;
+}
+.top-bar > div:last-of-type {
+  float: right;
+}
+.top-bar > div:last-of-type > div {
+  margin-left: 10px;
+}
 
+.icon {
+  position: relative;
+  display: inline-block;
+  background: #ffffff;
+  padding: 0px;
+  margin: 0px;
+  border-radius: 50%;
+  vertical-align: text-bottom;
+  z-index: 10;
+}
+.icon > img {
+  float: left;
+  width: 30px;
+  height: 30px;
+}
+.icon + .icon {
+  margin-left: -15px;
+  z-index: 5;
+}
+.title {
+  margin-left: 20px;
+  font-size: 30px;
+  line-height: 30px;
+}
 </style>
