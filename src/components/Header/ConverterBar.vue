8d1e9d42137eaf39a603a4e297b8bd832f42b2e1
Sam Wong <sampaksum1212@gmail.com>
2021-04-30 00:15:32 +0800

    Implement converterbar with pair data from graphql

--- src/components/Header/ConverterBar.vue
+++ src/components/Header/ConverterBar.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="converter-bar">
-    <div class="card"><span class="icon"><img v-bind:src="icon(this.$code1)" /></span>{{ format($code1, $code2, data.code1.value, data.code1.usd) }}</div>
-    <div class="card"><span class="icon"><img v-bind:src="icon(this.$code2)" /></span>{{ format($code2, $code1, data.code2.value, data.code2.usd) }}</div>
+  <div class="converter-bar" v-if="pairs">
+    <div class="card"><span class="icon"><img v-bind:src="icon(this.$code1)" /></span>{{ format($code1, $code2, getPrice($code1, pairs), data.code1.usd) }}</div>
+    <div class="card"><span class="icon"><img v-bind:src="icon(this.$code2)" /></span>{{ format($code2, $code1, getPrice($code2, pairs), data.code2.usd) }}</div>
   </div>
 </template>
 
 <script>
+import gql from 'graphql-tag'
+
+const GET_PAIR = gql`
+  query getPair($id: ID!) {
+    pairs (where: { id: $id }) {
+      id,
+      token0Price,
+      token1Price
+    }
+  }
+`;
+
+const GET_PAIR_VAR = {   
+  id: "0x06ad23978f67ae8cc76c54b74993b31a816bac2b"
+};
+
 export default {
   name: 'ConverterBar',
   props: {
@@ -13,7 +29,14 @@ export default {
   },
   methods: {
     format: (code1, code2, value1, value2) => `1 ${code1} = ${value1} ${code2} (US$${value2})`,
-    icon: (code) => require('@/assets/icons/' + code + '.png')
+    icon: (code) => require('@/assets/icons/' + code + '.png'),
+    getPrice: (code, pairs) => {
+      if (pairs && pairs.length > 0) {
+        let price = code === "CONV" ? parseFloat(pairs[0].token0Price) : code === "ETH" ? parseFloat(pairs[0].token1Price) : 0;
+        return price > 0 && price < 0.0001 ? "< 0.0001" : price.toFixed(0).replace(/(.)(?=(\d{3})+$)/g,'$1,');
+      }
+      return 0;
+    }
   },
   data() {
     return {
@@ -27,6 +50,12 @@ export default {
           usd: "0.11"
         }
       }
+    }
+  },
+  apollo: {
+    pairs: {
+      query: GET_PAIR,
+      variables: () => GET_PAIR_VAR
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import VueApollo from 'vue-apollo';
 
 Vue.config.productionTip = false
 
@@ -7,6 +12,28 @@ Vue.config.productionTip = false
 Vue.prototype.$code1 = "ETH"
 Vue.prototype.$code2 = "CONV"
 
+// HTTP connection to the API
+const httpLink = new HttpLink({
+  uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
 new Vue({
   render: h => h(App),
+  // inject apolloProvider here like vue-router or vuex
+  apolloProvider
 }).$mount('#app')
